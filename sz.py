940b33d14720fc5e7c1550a2ebe036dc7a776330
iasonkrom <iason.krom@gmail.com>
2023-08-22 14:46:28 +0300

    add sz.py from geohot

---
+++ sz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import itertools
+import os
+import token
+import tokenize
+
+from tabulate import tabulate
+
+TOKEN_WHITELIST = [token.OP, token.NAME, token.NUMBER, token.STRING]
+
+if __name__ == "__main__":
+    headers = ["Name", "Lines", "Tokens/Line"]
+    table = []
+    for path, subdirs, files in os.walk("src"):
+        for name in files:
+            if not name.endswith(".py"):
+                continue
+            filepath = os.path.join(path, name)
+            with tokenize.open(filepath) as file_:
+                tokens = [
+                    t
+                    for t in tokenize.generate_tokens(file_.readline)
+                    if t.type in TOKEN_WHITELIST
+                ]
+                token_count, line_count = len(tokens), len({t.start[0] for t in tokens})
+                table.append([filepath, line_count, token_count / line_count])
+
+    print(
+        tabulate(
+            [headers] + sorted(table, key=lambda x: -x[1]),
+            headers="firstrow",
+            floatfmt=".1f",
+        )
+        + "\n"
+    )
+
+    for dir_name, group in itertools.groupby(
+        sorted([(x[0].rsplit("/", 1)[0], x[1]) for x in table]), key=lambda x: x[0]
+    ):
+        print(f"{dir_name:30s} : {sum([x[1] for x in group]):6d}")
+
+    print(f"\ntotal line count: {sum([x[1] for x in table])}")

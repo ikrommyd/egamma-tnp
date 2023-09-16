6c0af7bb8e3b5c6ae25a07ab5657b18e900b7a80
iasonkrom <iason.krom@gmail.com>
2023-09-16 01:01:45 -0500

    move some methods to basetrigger

--- src/egamma_tnp/triggers/basetrigger.py
+++ src/egamma_tnp/triggers/basetrigger.py
@@ -1,6 +1,182 @@
 import os
 
+import dask_awkward as dak
+
 from egamma_tnp.utils.dataset import get_nanoevents_file
+
+
+def get_arrays(events, perform_tnp, *args, **kwargs):
+    p1, a1, p2, a2 = perform_tnp(events, *args, **kwargs)
+
+    pt_pass1 = dak.flatten(p1.pt)
+    pt_pass2 = dak.flatten(p2.pt)
+    pt_all1 = dak.flatten(a1.pt)
+    pt_all2 = dak.flatten(a2.pt)
+
+    eta_pass1 = dak.flatten(p1.eta)
+    eta_pass2 = dak.flatten(p2.eta)
+    eta_all1 = dak.flatten(a1.eta)
+    eta_all2 = dak.flatten(a2.eta)
+
+    phi_pass1 = dak.flatten(p1.phi)
+    phi_pass2 = dak.flatten(p2.phi)
+    phi_all1 = dak.flatten(a1.phi)
+    phi_all2 = dak.flatten(a2.phi)
+
+    return (
+        pt_pass1,
+        pt_pass2,
+        pt_all1,
+        pt_all2,
+        eta_pass1,
+        eta_pass2,
+        eta_all1,
+        eta_all2,
+        phi_pass1,
+        phi_pass2,
+        phi_all1,
+        phi_all2,
+    )
+
+
+def get_and_compute_arrays(events, perform_tnp, scheduler, progress, *args, **kwargs):
+    import dask
+    from dask.diagnostics import ProgressBar
+
+    (
+        pt_pass1,
+        pt_pass2,
+        pt_all1,
+        pt_all2,
+        eta_pass1,
+        eta_pass2,
+        eta_all1,
+        eta_all2,
+        phi_pass1,
+        phi_pass2,
+        phi_all1,
+        phi_all2,
+    ) = get_arrays(events, perform_tnp, *args, **kwargs)
+
+    if progress:
+        pbar = ProgressBar()
+        pbar.register()
+
+    res = dask.compute(
+        pt_pass1,
+        pt_pass2,
+        pt_all1,
+        pt_all2,
+        eta_pass1,
+        eta_pass2,
+        eta_all1,
+        eta_all2,
+        phi_pass1,
+        phi_pass2,
+        phi_all1,
+        phi_all2,
+        scheduler=scheduler,
+    )
+
+    if progress:
+        pbar.unregister()
+
+    return res
+
+
+def get_tnp_histograms(events, perform_tnp, *args, **kwargs):
+    import json
+    import os
+
+    import hist
+    from hist.dask import Hist
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    config_path = os.path.join(dir_path, "config.json")
+
+    with open(config_path) as f:
+        config = json.load(f)
+
+    ptbins = config["ptbins"]
+    etabins = config["etabins"]
+    phibins = config["phibins"]
+
+    (
+        pt_pass1,
+        pt_pass2,
+        pt_all1,
+        pt_all2,
+        eta_pass1,
+        eta_pass2,
+        eta_all1,
+        eta_all2,
+        phi_pass1,
+        phi_pass2,
+        phi_all1,
+        phi_all2,
+    ) = get_arrays(events, perform_tnp, *args, **kwargs)
+
+    ptaxis = hist.axis.Variable(ptbins, name="pt")
+    hpt_all = Hist(ptaxis)
+    hpt_pass = Hist(ptaxis)
+
+    etaaxis = hist.axis.Variable(etabins, name="eta")
+    heta_all = Hist(etaaxis)
+    heta_pass = Hist(etaaxis)
+
+    phiaxis = hist.axis.Variable(phibins, name="phi")
+    hphi_all = Hist(phiaxis)
+    hphi_pass = Hist(phiaxis)
+
+    hpt_pass.fill(pt_pass1)
+    hpt_pass.fill(pt_pass2)
+    hpt_all.fill(pt_all1)
+    hpt_all.fill(pt_all2)
+    heta_pass.fill(eta_pass1)
+    heta_pass.fill(eta_pass2)
+    heta_all.fill(eta_all1)
+    heta_all.fill(eta_all2)
+    hphi_pass.fill(phi_pass1)
+    hphi_pass.fill(phi_pass2)
+    hphi_all.fill(phi_all1)
+    hphi_all.fill(phi_all2)
+
+    return hpt_pass, hpt_all, heta_pass, heta_all, hphi_pass, hphi_all
+
+
+def get_and_compute_tnp_histograms(
+    events, perform_tnp, scheduler, progress, *args, **kwargs
+):
+    import dask
+    from dask.diagnostics import ProgressBar
+
+    (
+        hpt_pass,
+        hpt_all,
+        heta_pass,
+        heta_all,
+        hphi_pass,
+        hphi_all,
+    ) = get_tnp_histograms(events, perform_tnp, *args, **kwargs)
+
+    if progress:
+        pbar = ProgressBar()
+        pbar.register()
+
+    res = dask.compute(
+        hpt_pass,
+        hpt_all,
+        heta_pass,
+        heta_all,
+        hphi_pass,
+        hphi_all,
+        scheduler=scheduler,
+    )
+
+    if progress:
+        pbar.unregister()
+
+    return res
 
 
 class BaseTrigger:
@@ -12,6 +188,7 @@ class BaseTrigger:
     def __init__(
         self,
         names,
+        perform_tnp,
         goldenjson,
         toquery,
         redirect,
@@ -23,6 +200,7 @@ class BaseTrigger:
         extra_filter_args,
     ):
         self.names = names
+        self._perform_tnp = perform_tnp
         self.goldenjson = goldenjson
         self.events = None
         self._toquery = toquery
@@ -112,3 +290,118 @@ class BaseTrigger:
             permit_dask=True,
             **from_root_args,
         ).events()
+
+    def get_arrays(self, compute=False, scheduler=None, progress=True):
+        """Get the Pt and Eta arrays of the passing and all probes.
+        WARNING: Not recommended to be used for large datasets as the arrays can be very large.
+
+        Parameters
+        ----------
+            compute : bool, optional
+                Whether to return the computed arrays or the delayed arrays.
+                The default is False.
+            scheduler : str, optional
+                The dask scheduler to use. The default is None.
+                Only used if compute is True.
+            progress : bool, optional
+                Whether to show a progress bar if `compute` is True. The default is True.
+                Only used if compute is True and no distributed Client is used.
+
+        Returns
+        -------
+            pt_pass1: numpy.ndarray or dask_awkward.Array
+                The Pt array of the passing probes when the firsts electrons are the tags.
+            pt_pass2: numpy.ndarray or dask_awkward.Array
+                The Pt array of the passing probes when the seconds electrons are the tags.
+            pt_all1: numpy.ndarray or dask_awkward.Array
+                The Pt array of all probes when the firsts electrons are the tags.
+            pt_all2: numpy.ndarray or dask_awkward.Array
+                The Pt array of all probes when the seconds electrons are the tags.
+            eta_pass1: numpy.ndarray or dask_awkward.Array
+                The Eta array of the passing probes when the firsts electrons are the tags.
+            eta_pass2: numpy.ndarray or dask_awkward.Array
+                The Eta array of the passing probes when the seconds electrons are the tags.
+            eta_all1: numpy.ndarray or dask_awkward.Array
+                The Eta array of all probes when the firsts electrons are the tags.
+            eta_all2: numpy.ndarray or dask_awkward.Array
+                The Eta array of all probes when the seconds electrons are the tags.
+            phi_pass1: numpy.ndarray or dask_awkward.Array
+                The Phi array of the passing probes when the firsts electrons are the tags.
+            phi_pass2: numpy.ndarray or dask_awkward.Array
+                The Phi array of the passing probes when the seconds electrons are the tags.
+            phi_all1: numpy.ndarray or dask_awkward.Array
+                The Phi array of all probes when the firsts electrons are the tags.
+            phi_all2: numpy.ndarray or dask_awkward.Array
+                The Phi array of all probes when the seconds electrons are the tags.
+        """
+        if compute:
+            return get_and_compute_arrays(
+                events=self.events,
+                perform_tnp=self._perform_tnp,
+                pt=self.pt,
+                goldenjson=self.goldenjson,
+                scheduler=scheduler,
+                progress=progress,
+                extra_filter=self._extra_filter,
+                extra_filter_args=self._extra_filter_args,
+            )
+        else:
+            return get_arrays(
+                events=self.events,
+                perform_tnp=self._perform_tnp,
+                pt=self.pt,
+                goldenjson=self.goldenjson,
+                extra_filter=self._extra_filter,
+                extra_filter_args=self._extra_filter_args,
+            )
+
+    def get_tnp_histograms(self, compute=False, scheduler=None, progress=True):
+        """Get the Pt and Eta histograms of the passing and all probes.
+
+        Parameters
+        ----------
+            compute : bool, optional
+                Whether to return the computed hist.Hist histograms or the delayed hist.dask.Hist histograms.
+                The default is False.
+            scheduler : str, optional
+                The dask scheduler to use. The default is None.
+                Only used if compute is True.
+            progress : bool, optional
+                Whether to show a progress bar if `compute` is True. The default is True.
+                Only used if compute is True and no distributed Client is used.
+
+        Returns
+        -------
+            hpt_pass: hist.Hist or hist.dask.Hist
+                The Pt histogram of the passing probes.
+            hpt_all: hist.Hist or hist.dask.Hist
+                The Pt histogram of all probes.
+            heta_pass: hist.Hist or hist.dask.Hist
+                The Eta histogram of the passing probes.
+            heta_all: hist.Hist or hist.dask.Hist
+                The Eta histogram of all probes.
+            hphi_pass: hist.Hist or hist.dask.Hist
+                The Phi histogram of the passing probes.
+            hphi_all: hist.Hist or hist.dask.Hist
+                The Phi histogram of all probes.
+        """
+        if compute:
+            return get_and_compute_tnp_histograms(
+                events=self.events,
+                perform_tnp=self._perform_tnp,
+                pt=self.pt,
+                goldenjson=self.goldenjson,
+                scheduler=scheduler,
+                progress=progress,
+                extra_filter=self._extra_filter,
+                extra_filter_args=self._extra_filter_args,
+            )
+        else:
+            return get_tnp_histograms(
+                events=self.events,
+                perform_tnp=self._perform_tnp,
+                pt=self.pt,
+                goldenjson=self.goldenjson,
+                extra_filter=self._extra_filter,
+                extra_filter_args=self._extra_filter_args,
+            )

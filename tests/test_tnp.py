cff2c43439d6248affa1cedacd3ab4ab2b0713eb
iasonkrom <iason.krom@gmail.com>
2023-09-20 21:30:57 -0500

    update tests with tiny MC root file

--- tests/test_tnp.py
+++ tests/test_tnp.py
@@ -9,12 +9,9 @@ from egamma_tnp.triggers import ElePt_WPTight_Gsf
 @pytest.mark.parametrize("preprocess", [False, True])
 def test_local_compute(scheduler, preprocess):
     tag_n_probe = ElePt_WPTight_Gsf(
-        [
-            os.path.abspath("root_files/Egamma0.root"),
-            os.path.abspath("root_files/Egamma1.root"),
-        ],
+        os.path.abspath("tests/samples/DYto2E.root"),
         32,
-        goldenjson="json/Cert_Collisions2023_366442_370790_Golden.json",
+        goldenjson=None,
         toquery=False,
         redirect=False,
         preprocess=preprocess,
@@ -30,12 +27,12 @@ def test_local_compute(scheduler, preprocess):
         hpti_all,
     ) = tag_n_probe.get_tnp_histograms(compute=True, scheduler=scheduler, progress=True)
 
-    assert hpt_pass.sum(flow=True) == 14598.0
-    assert hpt_all.sum(flow=True) == 16896.0
-    assert heta_pass.sum(flow=True) == 14598.0
-    assert heta_all.sum(flow=True) == 16896.0
-    assert hphi_pass.sum(flow=True) == 14598.0
-    assert hpti_all.sum(flow=True) == 16896.0
+    assert hpt_pass.sum(flow=True) == 144.0
+    assert hpt_all.sum(flow=True) == 171.0
+    assert heta_pass.sum(flow=True) == 144.0
+    assert heta_all.sum(flow=True) == 171.0
+    assert hphi_pass.sum(flow=True) == 144.0
+    assert hpti_all.sum(flow=True) == 171.0
 
     assert hpt_pass.values(flow=True)[0] == 0.0
     assert hpt_all.values(flow=True)[0] == 0.0
@@ -50,12 +47,9 @@ def test_distributed_compute(preprocess):
     from distributed import Client
 
     tag_n_probe = ElePt_WPTight_Gsf(
-        [
-            os.path.abspath("root_files/Egamma0.root"),
-            os.path.abspath("root_files/Egamma1.root"),
-        ],
+        os.path.abspath("tests/samples/DYto2E.root"),
         32,
-        goldenjson="json/Cert_Collisions2023_366442_370790_Golden.json",
+        goldenjson=None,
         toquery=False,
         redirect=False,
         preprocess=preprocess,
@@ -72,12 +66,12 @@ def test_distributed_compute(preprocess):
             hpti_all,
         ) = tag_n_probe.get_tnp_histograms(compute=True, scheduler=None, progress=True)
 
-        assert hpt_pass.sum(flow=True) == 14598.0
-        assert hpt_all.sum(flow=True) == 16896.0
-        assert heta_pass.sum(flow=True) == 14598.0
-        assert heta_all.sum(flow=True) == 16896.0
-        assert hphi_pass.sum(flow=True) == 14598.0
-        assert hpti_all.sum(flow=True) == 16896.0
+        assert hpt_pass.sum(flow=True) == 144.0
+        assert hpt_all.sum(flow=True) == 171.0
+        assert heta_pass.sum(flow=True) == 144.0
+        assert heta_all.sum(flow=True) == 171.0
+        assert hphi_pass.sum(flow=True) == 144.0
+        assert hpti_all.sum(flow=True) == 171.0
 
         assert hpt_pass.values(flow=True)[0] == 0.0
         assert hpt_all.values(flow=True)[0] == 0.0

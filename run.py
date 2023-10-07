f16484ef1009cef53ef12561d794a323530232d8
Iason Krommydas <iason.krom@gmail.com>
2023-10-07 03:47:25 -0500

    run.py is better this way

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import dask
 import uproot
 from distributed import Client
 
+import egamma_tnp
 from egamma_tnp.triggers import ElePt_CaloIdVT_GsfTrkIdT
 
 
@@ -13,6 +15,42 @@ def l1_filter(events, l1_seeds):
     return events
 
 
+egamma_tnp.config.set(
+    "ptbins",
+    [
+        5,
+        10,
+        15,
+        20,
+        22,
+        26,
+        28,
+        30,
+        32,
+        34,
+        36,
+        38,
+        40,
+        45,
+        50,
+        60,
+        80,
+        100,
+        105,
+        110,
+        115,
+        120,
+        125,
+        130,
+        135,
+        140,
+        145,
+        150,
+        250,
+        400,
+    ],
+)
+
 if __name__ == "__main__":
     client = Client()
 
@@ -24,8 +62,20 @@ if __name__ == "__main__":
 
     tag_n_probe = ElePt_CaloIdVT_GsfTrkIdT(
         [
+            "/EGamma0/Run2023B-PromptNanoAODv11p9_v1-v1/NANOAOD",
+            "/EGamma1/Run2023B-PromptNanoAODv11p9_v1-v1/NANOAOD",
+            "/EGamma0/Run2023C-PromptNanoAODv11p9_v1-v1/NANOAOD",
+            "/EGamma1/Run2023C-PromptNanoAODv11p9_v1-v1/NANOAOD",
+            "/EGamma0/Run2023C-PromptNanoAODv12_v2-v2/NANOAOD",
+            "/EGamma1/Run2023C-PromptNanoAODv12_v2-v2/NANOAOD",
             "/EGamma0/Run2023C-PromptNanoAODv12_v3-v1/NANOAOD",
             "/EGamma1/Run2023C-PromptNanoAODv12_v3-v1/NANOAOD",
+            "/EGamma0/Run2023C-PromptNanoAODv12_v4-v1/NANOAOD",
+            "/EGamma1/Run2023C-PromptNanoAODv12_v4-v1/NANOAOD",
+            "/EGamma0/Run2023D-PromptReco-v1/NANOAOD",
+            "/EGamma1/Run2023D-PromptReco-v1/NANOAOD",
+            "/EGamma0/Run2023D-PromptReco-v2/NANOAOD",
+            "/EGamma1/Run2023D-PromptReco-v2/NANOAOD",
         ],
         115,
         goldenjson="json/Cert_Collisions2023_366442_370790_Golden.json",
@@ -33,21 +83,19 @@ if __name__ == "__main__":
         redirect=False,
         preprocess=True,
         preprocess_args={"maybe_step_size": 100_000},
-        extra_filter=None,
-        extra_filter_args={"l1_seeds": ["SingleIsoEG30er2p5"]},
     )
 
     print("Starting to load events")
     tag_n_probe.load_events(from_root_args={"uproot_options": {"timeout": 120}})
     print(tag_n_probe)
 
-    res = tag_n_probe.get_tnp_histograms(compute=True, scheduler=None, progress=True)
-    hpt_pass, hpt_all, heta_pass, heta_all, hphi_pass, hphi_all = res
+    res = tag_n_probe.get_tnp_histograms()
+    hpt_pass, hpt_all, heta_pass, heta_all, hphi_pass, hphi_all = dask.compute(*res)
 
     print(f"Passing probes: {hpt_pass.sum(flow=True)}")
     print(f"All probes: {hpt_all.sum(flow=True)}")
 
-    with uproot.recreate("root_files/Run2023Cv3.root") as file:
+    with uproot.recreate("root_files/Run2023.root") as file:
         file["hpt_pass"] = hpt_pass
         file["hpt_all"] = hpt_all
         file["heta_pass"] = heta_pass

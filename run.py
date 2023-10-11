e2a44e756c94f65291fc3da37a851b9a098e5bfb
Iason Krommydas <iason.krom@gmail.com>
2023-10-11 15:49:43 -0500

    modify run.py scripy for DP note

--- run.py
+++ run.py
@@ -1,19 +1,8 @@
-import dask
 import uproot
 from distributed import Client
 
 import egamma_tnp
-from egamma_tnp.triggers import ElePt_CaloIdVT_GsfTrkIdT
-
-
-def l1_filter(events, l1_seeds):
-    if isinstance(l1_seeds, str):
-        l1_seeds = [l1_seeds]
-    for seed in l1_seeds:
-        mask = events.L1[seed]
-        events = events[mask]
-    return events
-
+from egamma_tnp.triggers import ElePt_WPTight_Gsf
 
 egamma_tnp.config.set(
     "ptbins",
@@ -36,20 +25,47 @@ egamma_tnp.config.set(
         60,
         80,
         100,
-        105,
-        110,
-        115,
-        120,
-        125,
-        130,
-        135,
-        140,
-        145,
         150,
         250,
         400,
     ],
 )
+
+samples2022 = {
+    "2022C": ["/EGamma/Run2022C-PromptNanoAODv10_v1-v1/NANOAOD"],
+    "2022D": [
+        "/EGamma/Run2022D-PromptNanoAODv10_v1-v1/NANOAOD",
+        "/EGamma/Run2022D-PromptNanoAODv10_v2-v1/NANOAOD",
+    ],
+    "2022E": ["/EGamma/Run2022E-PromptNanoAODv10_v1-v2/NANOAOD"],
+    "2022F": ["/EGamma/Run2022F-PromptNanoAODv10_v1-v2/NANOAOD"],
+    "2022G": ["/EGamma/Run2022G-PromptNanoAODv10_v1-v1/NANOAOD"],
+}
+
+samples2023 = {
+    "2023B": [
+        "/EGamma0/Run2023B-PromptNanoAODv11p9_v1-v1/NANOAOD",
+        "/EGamma1/Run2023B-PromptNanoAODv11p9_v1-v1/NANOAOD",
+    ],
+    "2023C": [
+        "/EGamma0/Run2023C-PromptNanoAODv11p9_v1-v1/NANOAOD",
+        "/EGamma1/Run2023C-PromptNanoAODv11p9_v1-v1/NANOAOD",
+        "/EGamma0/Run2023C-PromptNanoAODv12_v2-v2/NANOAOD",
+        "/EGamma1/Run2023C-PromptNanoAODv12_v2-v2/NANOAOD",
+        "/EGamma0/Run2023C-PromptNanoAODv12_v3-v1/NANOAOD",
+        "/EGamma1/Run2023C-PromptNanoAODv12_v3-v1/NANOAOD",
+        "/EGamma0/Run2023C-PromptNanoAODv12_v4-v1/NANOAOD",
+        "/EGamma1/Run2023C-PromptNanoAODv12_v4-v1/NANOAOD",
+    ],
+    "2023D": [
+        "/EGamma0/Run2023D-PromptReco-v1/NANOAOD",
+        "/EGamma1/Run2023D-PromptReco-v1/NANOAOD",
+        "/EGamma0/Run2023D-PromptReco-v2/NANOAOD",
+        "/EGamma1/Run2023D-PromptReco-v2/NANOAOD",
+    ],
+}
+
+pt = 32
 
 if __name__ == "__main__":
     client = Client()
@@ -60,45 +76,50 @@ if __name__ == "__main__":
     # cluster.adapt(minimum=1, maximum=100)
     # client = Client(cluster)
 
-    tag_n_probe = ElePt_CaloIdVT_GsfTrkIdT(
-        [
-            "/EGamma0/Run2023B-PromptNanoAODv11p9_v1-v1/NANOAOD",
-            "/EGamma1/Run2023B-PromptNanoAODv11p9_v1-v1/NANOAOD",
-            "/EGamma0/Run2023C-PromptNanoAODv11p9_v1-v1/NANOAOD",
-            "/EGamma1/Run2023C-PromptNanoAODv11p9_v1-v1/NANOAOD",
-            "/EGamma0/Run2023C-PromptNanoAODv12_v2-v2/NANOAOD",
-            "/EGamma1/Run2023C-PromptNanoAODv12_v2-v2/NANOAOD",
-            "/EGamma0/Run2023C-PromptNanoAODv12_v3-v1/NANOAOD",
-            "/EGamma1/Run2023C-PromptNanoAODv12_v3-v1/NANOAOD",
-            "/EGamma0/Run2023C-PromptNanoAODv12_v4-v1/NANOAOD",
-            "/EGamma1/Run2023C-PromptNanoAODv12_v4-v1/NANOAOD",
-            "/EGamma0/Run2023D-PromptReco-v1/NANOAOD",
-            "/EGamma1/Run2023D-PromptReco-v1/NANOAOD",
-            "/EGamma0/Run2023D-PromptReco-v2/NANOAOD",
-            "/EGamma1/Run2023D-PromptReco-v2/NANOAOD",
-        ],
-        115,
-        goldenjson="json/Cert_Collisions2023_366442_370790_Golden.json",
-        toquery=True,
-        redirect=False,
-        preprocess=True,
-        preprocess_args={"maybe_step_size": 100_000},
-    )
+    for name, samples in samples2022.items():
+        goldenjson = (
+            "json/Cert_Collisions2023_366442_370790_Golden.json"
+            if "2023" in name
+            else "json/Cert_Collisions2022_355100_362760_Golden.json"
+        )
 
-    print("Starting to load events")
-    tag_n_probe.load_events(from_root_args={"uproot_options": {"timeout": 120}})
-    print(tag_n_probe)
+        tag_n_probe = ElePt_WPTight_Gsf(
+            samples,
+            pt,
+            goldenjson=goldenjson,
+            toquery=True,
+            redirect=False,
+            preprocess=False,
+            preprocess_args={"maybe_step_size": 100_000},
+        )
 
-    res = tag_n_probe.get_tnp_histograms()
-    hpt_pass, hpt_all, heta_pass, heta_all, hphi_pass, hphi_all = dask.compute(*res)
+        print("Starting to load events")
+        tag_n_probe.load_events(from_root_args={"uproot_options": {"timeout": 120}})
+        print(tag_n_probe)
 
-    print(f"Passing probes: {hpt_pass.sum(flow=True)}")
-    print(f"All probes: {hpt_all.sum(flow=True)}")
+        res = tag_n_probe.get_tnp_histograms(
+            eta_regions={
+                "barrel": [0.0, 1.4442],
+                "endcap_loweta": [1.566, 2.0],
+                "endcap_higheta": [2.0, 2.5],
+            },
+            compute=False,
+        )
 
-    with uproot.recreate("root_files/Run2023.root") as file:
-        file["hpt_pass"] = hpt_pass
-        file["hpt_all"] = hpt_all
-        file["heta_pass"] = heta_pass
-        file["heta_all"] = heta_all
-        file["hphi_pass"] = hphi_pass
-        file["hphi_all"] = hphi_all
+        hist_names = [
+            "hpt_pass",
+            "hpt_all",
+            "heta_pass",
+            "heta_all",
+            "hphi_pass",
+            "hphi_all",
+        ]
+        with uproot.recreate(f"root_files/Run{name}_Ele{pt}.root") as f:
+            for region_name, hists in res.items():
+                for i, h in enumerate(hists):
+                    print(
+                        f"Run{name}_Ele{pt}_{region_name}_{hist_names[i]} sum : {h.sum(flow=True)}"
+                    )
+                    f[f"{region_name}/{hist_names[i]}"] = h
+
+        print(f"\nFinished running Run{name}_Ele{pt}\n")

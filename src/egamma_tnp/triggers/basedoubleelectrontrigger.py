2648d7c4e03826ca42f197181d6c8d284a6e5b76
iasonkrom <iason.krom@gmail.com>
2023-12-12 17:37:37 +0100

    merge some methods into a new BaseTrigger class

--- src/egamma_tnp/triggers/basedoubleelectrontrigger.py
+++ src/egamma_tnp/triggers/basedoubleelectrontrigger.py
@@ -1,9 +1,6 @@
-import json
-import os
-
 import dask_awkward as dak
 
-from egamma_tnp.utils.dataset import get_nanoevents_file
+from .basetrigger import BaseTrigger
 
 
 def _get_arrays_on_leg(events, perform_tnp, **kwargs):
@@ -348,7 +345,7 @@ def _get_and_compute_both_leg_tnp_histograms(
     return res
 
 
-class BaseDoubleElectronTrigger:
+class BaseDoubleElectronTrigger(BaseTrigger):
     """BaseDoubleElectronTrigger class for HLT Trigger efficiency from NanoAOD.
 
     This class holds the basic methods for all the Tag and Probe classes for different double electron triggers.
@@ -372,111 +369,23 @@ class BaseDoubleElectronTrigger:
         extra_filter,
         extra_filter_args,
     ):
-        self.names = names
-        self._perform_tnp = perform_tnp
+        super().__init__(
+            names,
+            perform_tnp,
+            avoid_ecal_transition_tags,
+            avoid_ecal_transition_probes,
+            goldenjson,
+            toquery,
+            redirect,
+            custom_redirector,
+            invalid,
+            preprocess,
+            preprocess_args,
+            extra_filter,
+            extra_filter_args,
+        )
         self.pt1 = pt1
         self.pt2 = pt2
-        self.avoid_ecal_transition_tags = avoid_ecal_transition_tags
-        self.avoid_ecal_transition_probes = avoid_ecal_transition_probes
-        self.goldenjson = goldenjson
-        self.events = None
-        self._toquery = toquery
-        self._redirect = redirect
-        self._custom_redirector = custom_redirector
-        self._invalid = invalid
-        self._preprocess = preprocess
-        self._preprocess_args = preprocess_args
-        self._extra_filter = extra_filter
-        self._extra_filter_args = extra_filter_args
-
-        self.file = get_nanoevents_file(
-            self.names,
-            toquery=self._toquery,
-            redirect=self._redirect,
-            custom_redirector=self._custom_redirector,
-            invalid=self._invalid,
-            preprocess=self._preprocess,
-            preprocess_args=self._preprocess_args,
-        )
-
-        if goldenjson is not None and not os.path.exists(goldenjson):
-            raise FileNotFoundError(f"Golden JSON {goldenjson} does not exist.")
-
-        config_path = os.path.join(
-            os.path.dirname(__file__), "..", "config/runtime_config.json"
-        )
-        with open(config_path) as f:
-            self._bins = json.load(f)
-
-    def remove_bad_xrootd_files(self, keys):
-        """Remove bad xrootd files from self.file.
-
-        Parameters
-        ----------
-            keys : str or list of str
-                The keys of self.file to remove.
-        """
-        if isinstance(keys, str):
-            keys = [keys]
-        for key in keys:
-            try:
-                self.file.pop(key)
-            except KeyError:
-                pass
-
-    def redirect_files(self, keys, redirectors="root://cmsxrootd.fnal.gov/"):
-        """Redirect the files in self.file.
-
-        Parameters
-        ----------
-            keys : str or list of str
-                The keys of self.file to redirect.
-            redirectors : str or list of str, optional
-                The redirectors to use. The default is "root://cmsxrootd.fnal.gov/".
-                If multiple keys are given, then either one redirector or the same number of redirectors as keys must be given.
-        """
-        from egamma_tnp.utils import redirect_files
-
-        if isinstance(keys, str):
-            keys = [keys]
-        if isinstance(redirectors, str) or (
-            isinstance(redirectors, list) and len(redirectors) == 1
-        ):
-            redirectors = (
-                [redirectors] * len(keys)
-                if isinstance(redirectors, str)
-                else redirectors * len(keys)
-            )
-        if (len(keys) > 1 and (len(redirectors) != 1)) and (
-            len(keys) != len(redirectors)
-        ):
-            raise ValueError(
-                f"If multiple keys are given, then either one redirector or the same number of redirectors as keys must be given."
-                f"Got {len(keys)} keys and {len(redirectors)} redirectors."
-            )
-        for key, redirector in zip(keys, redirectors):
-            isrucio = True if key[:7] == "root://" else False
-            newkey = redirect_files(key, redirector=redirector, isrucio=isrucio).pop()
-            self.file[newkey] = self.file.pop(key)
-
-    def load_events(self, from_root_args=None):
-        """Load the events from the names.
-
-        Parameters
-        ----------
-            from_root_args : dict, optional
-                Extra arguments to pass to coffea.nanoevents.NanoEventsFactory.from_root().
-                The default is {}.
-        """
-        from coffea.nanoevents import NanoEventsFactory
-
-        if from_root_args is None:
-            from_root_args = {}
-
-        self.events = NanoEventsFactory.from_root(
-            self.file,
-            **from_root_args,
-        ).events()
 
     def get_arrays(self, leg="both", compute=False, scheduler=None, progress=True):
         """Get the Pt and Eta arrays of the passing and all probes.

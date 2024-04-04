64f9fd70eeac05c0660152ea1372bfaa8fc0dc70
iasonkrom <iason.krom@gmail.com>
2024-04-04 17:48:49 -0500

    add mll histogramming function

--- src/egamma_tnp/utils/histogramming.py
+++ src/egamma_tnp/utils/histogramming.py
@@ -169,6 +169,157 @@ def fill_tnp_histograms(
     return histograms
 
 
+def fill_mll_histograms(
+    passing_probes,
+    all_probes,
+    plateau_cut=None,
+    eta_regions_pt=None,
+    eta_regions_eta=None,
+    eta_regions_phi=None,
+    delayed=True,
+):
+    """Get the 2D histograms of Pt, Eta and Phi vs mll of the passing and all probes.
+
+    Parameters
+    ----------
+        plateau_cut : int or float, optional
+            The Pt threshold to use to ensure that we are on the efficiency plateau for eta and phi histograms.
+            The default None, meaning that no extra cut is applied and the activation region is included in those histograms.
+        eta_regions_pt : dict, optional
+            A dictionary of the form `{"name": [etamin, etamax], ...}`
+            where name is the name of the region and etamin and etamax are the absolute eta bounds.
+            The Pt histograms will be split into those eta regions.
+            The default is to avoid the ECAL transition region meaning |eta| < 1.4442 or 1.566 < |eta| < 2.5.
+        eta_regions_eta : dict, optional
+            A dictionary of the form `{"name": [etamin, etamax], ...}`
+            where name is the name of the region and etamin and etamax are the absolute eta bounds.
+            The Eta histograms will be split into those eta regions.
+            The default is to use the entire |eta| < 2.5 region.
+        eta_regions_phi : dict, optional
+            A dictionary of the form `{"name": [etamin, etamax], ...}`
+            where name is the name of the region and etamin and etamax are the absolute eta bounds.
+            The Phi histograms will be split into those eta regions.
+            The default is to use the entire |eta| < 2.5 region.
+        delayed : bool, optional
+            Whether the probes arrays are delayed (dask-awkward) or not.
+            The default is True.
+
+    Returns
+    -------
+        histograms : dict
+            A dictionary of the form `{"var": {"name": {"passing": passing_probes, "all": all_probes}, ...}, ...}`
+            where `"var"` can be `"pt"`, `"eta"`, or `"phi"`.
+            Each `"name"` is the name of eta region specified by the user.
+            `passing_probes` and `all_probes` are `hist.Hist` or `hist.dask.Hist` objects.
+            These are the histograms of the passing and all probes respectively.
+    """
+    import hist
+
+    if delayed:
+        from hist.dask import Hist
+    else:
+        from hist import Hist
+
+    import egamma_tnp
+
+    if plateau_cut is None:
+        plateau_cut = 0
+    if eta_regions_pt is None:
+        eta_regions_pt = {
+            "barrel": [0.0, 1.4442],
+            "endcap": [1.566, 2.5],
+        }
+    if eta_regions_eta is None:
+        eta_regions_eta = {"entire": [0.0, 2.5]}
+    if eta_regions_phi is None:
+        eta_regions_phi = {"entire": [0.0, 2.5]}
+
+    ptbins = egamma_tnp.config.get("ptbins")
+    etabins = egamma_tnp.config.get("etabins")
+    phibins = egamma_tnp.config.get("phibins")
+
+    pt_pass = passing_probes.pt
+    pt_all = all_probes.pt
+    eta_pass = passing_probes.eta
+    eta_all = all_probes.eta
+    phi_pass = passing_probes.phi
+    phi_all = all_probes.phi
+    mll_pass = passing_probes.mass
+    mll_all = all_probes.mass
+
+    histograms = {}
+    histograms["pt"] = {}
+    histograms["eta"] = {}
+    histograms["phi"] = {}
+
+    plateau_mask_pass = pt_pass > plateau_cut
+    plateau_mask_all = pt_all > plateau_cut
+
+    for name_pt, region_pt in eta_regions_pt.items():
+        eta_mask_pt_pass = (abs(eta_pass) > region_pt[0]) & (
+            abs(eta_pass) < region_pt[1]
+        )
+        eta_mask_pt_all = (abs(eta_all) > region_pt[0]) & (abs(eta_all) < region_pt[1])
+        hpt_pass = Hist(
+            hist.axis.Variable(ptbins, name=f"hpt_{name_pt}", label="Pt [GeV]"),
+            hist.axis.Regular(80, 50, 130, name="mll", label="mll [GeV]"),
+        )
+        hpt_all = Hist(
+            hist.axis.Variable(ptbins, name=f"hpt_{name_pt}", label="Pt [GeV]"),
+            hist.axis.Regular(80, 50, 130, name="mll", label="mll [GeV]"),
+        )
+        hpt_pass.fill(pt_pass[eta_mask_pt_pass], mll_pass[eta_mask_pt_pass])
+        hpt_all.fill(pt_all[eta_mask_pt_all], mll_all[eta_mask_pt_all])
+
+        histograms["pt"][name_pt] = {"passing": hpt_pass, "all": hpt_all}
+
+    for name_eta, region_eta in eta_regions_eta.items():
+        eta_mask_eta_pass = (abs(eta_pass) > region_eta[0]) & (
+            abs(eta_pass) < region_eta[1]
+        )
+        eta_mask_eta_all = (abs(eta_all) > region_eta[0]) & (
+            abs(eta_all) < region_eta[1]
+        )
+        heta_pass = Hist(
+            hist.axis.Variable(etabins, name=f"heta_{name_eta}", label="eta"),
+            hist.axis.Regular(80, 50, 130, name="mll", label="mll [GeV]"),
+        )
+        heta_all = Hist(
+            hist.axis.Variable(etabins, name=f"heta_{name_eta}", label="eta"),
+            hist.axis.Regular(80, 50, 130, name="mll", label="mll [GeV]"),
+        )
+        eta_mask_pass = plateau_mask_pass & eta_mask_eta_pass
+        eta_mask_all = plateau_mask_all & eta_mask_eta_all
+        heta_pass.fill(eta_pass[eta_mask_pass], mll_pass[eta_mask_pass])
+        heta_all.fill(eta_all[eta_mask_all], mll_all[eta_mask_all])
+
+        histograms["eta"][name_eta] = {"passing": heta_pass, "all": heta_all}
+
+    for name_phi, region_phi in eta_regions_phi.items():
+        eta_mask_phi_pass = (abs(eta_pass) > region_phi[0]) & (
+            abs(eta_pass) < region_phi[1]
+        )
+        eta_mask_phi_all = (abs(eta_all) > region_phi[0]) & (
+            abs(eta_all) < region_phi[1]
+        )
+        hphi_pass = Hist(
+            hist.axis.Variable(phibins, name=f"hphi_{name_phi}", label="phi"),
+            hist.axis.Regular(80, 50, 130, name="mll", label="mll [GeV]"),
+        )
+        hphi_all = Hist(
+            hist.axis.Variable(phibins, name=f"hphi_{name_phi}", label="phi"),
+            hist.axis.Regular(80, 50, 130, name="mll", label="mll [GeV]"),
+        )
+        phi_mask_pass = plateau_mask_pass & eta_mask_phi_pass
+        phi_mask_all = plateau_mask_all & eta_mask_phi_all
+        hphi_pass.fill(phi_pass[phi_mask_pass], mll_pass[phi_mask_pass])
+        hphi_all.fill(phi_all[phi_mask_all], mll_all[phi_mask_all])
+
+        histograms["phi"][name_phi] = {"passing": hphi_pass, "all": hphi_all}
+
+    return histograms
+
+
 def save_hists(path, res):
     """Save histograms to a ROOT file.
 

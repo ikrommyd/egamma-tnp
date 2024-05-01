22dc7e19930ef230fbfe0ea55d32d8a9028e8fb8
Iason Krommydas <iason.krom@gmail.com>
2024-05-01 14:38:43 +0300

    remove duplicate test func

--- tests/test_histogramming_ntuples.py
+++ tests/test_histogramming_ntuples.py
@@ -1,16 +1,8 @@
 import os
 
-import dask_awkward as dak
 import numpy as np
-import uproot
 
 from egamma_tnp import TagNProbeFromNTuples
-from egamma_tnp.utils import (
-    fill_nd_cutncount_histograms,
-    fill_nd_mll_histograms,
-    fill_pt_eta_phi_cutncount_histograms,
-    fill_pt_eta_phi_mll_histograms,
-)
 
 fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples.root"): "fitter_tree"}}}
 
@@ -96,85 +88,6 @@ def test_histogramming_default_vars():
     assert_histograms_equal(hmll1d["eta"]["entire"]["failing"], hmll3d["failing"][35j::sum, -2.5j:2.5j, sum, :], flow=False)
     assert_histograms_equal(hmll1d["phi"]["entire"]["passing"], hmll3d["passing"][35j::sum, -2.5j:2.5j:sum, :, :], flow=False)
     assert_histograms_equal(hmll1d["phi"]["entire"]["failing"], hmll3d["failing"][35j::sum, -2.5j:2.5j:sum, :, :], flow=False)
-
-
-def test_histogramming_funcs_custom_vars():
-    import egamma_tnp
-
-    events = uproot.dask({os.path.abspath("tests/samples/TnPNTuples.root"): "fitter_tree"})
-
-    passing_probe_evens = events[events.passHltEle30WPTightGsf == 1]
-    failing_probe_evens = events[events.passHltEle30WPTightGsf == 0]
-
-    passing_probes = dak.zip(
-        {
-            "pt": passing_probe_evens.el_pt,
-            "eta": passing_probe_evens.el_eta,
-            "phi": passing_probe_evens.el_phi,
-            "r9": passing_probe_evens.el_r9,
-            "pair_mass": passing_probe_evens.pair_mass,
-        }
-    ).compute()
-    failing_probes = dak.zip(
-        {
-            "pt": failing_probe_evens.el_pt,
-            "eta": failing_probe_evens.el_eta,
-            "phi": failing_probe_evens.el_phi,
-            "r9": failing_probe_evens.el_r9,
-            "pair_mass": failing_probe_evens.pair_mass,
-        }
-    ).compute()
-
-    egamma_tnp.config.set("r9_bins", np.linspace(0.1, 1.05, 100).tolist())
-
-    hcnc1d = fill_pt_eta_phi_cutncount_histograms(
-        passing_probes,
-        failing_probes,
-        eta_regions_pt={
-            "barrel": [0.0, 1.4442],
-            "endcap_loweta": [1.566, 2.0],
-            "endcap_higheta": [2.0, 2.5],
-        },
-        plateau_cut=0,
-        vars=["pt", "eta", "phi"],
-        delayed=False,
-    )
-    hmll1d = fill_pt_eta_phi_mll_histograms(
-        passing_probes,
-        failing_probes,
-        vars=["pt", "eta", "phi"],
-        delayed=False,
-    )
-    hcnc3d = fill_nd_cutncount_histograms(
-        passing_probes,
-        failing_probes,
-        vars=["eta", "r9"],
-        delayed=False,
-    )
-    hmll3d = fill_nd_mll_histograms(
-        passing_probes,
-        failing_probes,
-        vars=["eta", "r9"],
-        delayed=False,
-    )
-
-    assert_histograms_equal(hcnc1d["eta"]["entire"]["passing"], hcnc3d["passing"][-2.5j:2.5j, sum], flow=False)
-    assert_histograms_equal(hcnc1d["eta"]["entire"]["failing"], hcnc3d["failing"][-2.5j:2.5j, sum], flow=False)
-
-    assert_histograms_equal(hmll1d["eta"]["entire"]["passing"], hmll3d["passing"][-2.5j:2.5j, sum, :], flow=False)
-    assert_histograms_equal(hmll1d["eta"]["entire"]["failing"], hmll3d["failing"][-2.5j:2.5j, sum, :], flow=False)
-
-    assert_histograms_equal(hcnc1d["eta"]["entire"]["passing"], hmll3d["passing"][-2.5j:2.5j, sum, sum], flow=False)
-    assert_histograms_equal(hcnc1d["eta"]["entire"]["failing"], hmll3d["failing"][-2.5j:2.5j, sum, sum], flow=False)
-
-    assert_histograms_equal(hcnc1d["eta"]["entire"]["passing"], hmll1d["eta"]["entire"]["passing"][:, sum], flow=False)
-    assert_histograms_equal(hcnc1d["eta"]["entire"]["failing"], hmll1d["eta"]["entire"]["failing"][:, sum], flow=False)
-
-    assert_histograms_equal(hmll1d["eta"]["entire"]["passing"][:, sum], hcnc3d["passing"][-2.5j:2.5j, sum], flow=False)
-    assert_histograms_equal(hmll1d["eta"]["entire"]["failing"][:, sum], hcnc3d["failing"][-2.5j:2.5j, sum], flow=False)
-
-    assert_histograms_equal(hcnc3d["passing"], hmll3d["passing"][..., sum], flow=False)
-    assert_histograms_equal(hcnc3d["failing"], hmll3d["failing"][..., sum], flow=False)
 
 
 def test_histogramming_custom_vars():

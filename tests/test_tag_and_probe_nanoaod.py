36efb77b34b44750d3767598c164f8560f7ca055
Iason Krommydas <iason.krom@gmail.com>
2024-06-06 01:48:52 +0200

    test T&P against dummy functions

--- tests/test_tag_and_probe_nanoaod.py
+++ tests/test_tag_and_probe_nanoaod.py
@@ -3,204 +3,59 @@ from __future__ import annotations
 import os
 
 import pytest
-from coffea.dataset_tools import preprocess
+from coffea.nanoevents import NanoEventsFactory
+from dask_awkward.lib.testutils import assert_eq
+from dummy_tag_and_probe_nanoaod import tag_and_probe_electrons, tag_and_probe_photons
 
-from egamma_tnp.triggers import ElePt_WPTight_Gsf
+from egamma_tnp import ElectronTagNProbeFromNanoAOD, PhotonTagNProbeFromNanoAOD
 
 
-@pytest.mark.parametrize("do_preprocess", [True, False])
-@pytest.mark.parametrize("allow_read_errors_with_report", [True, False])
-def test_without_compute(do_preprocess, allow_read_errors_with_report):
-    if allow_read_errors_with_report:
-        fileset = {
-            "sample": {
-                "files": {
-                    os.path.abspath("tests/samples/DYto2E.root"): "Events",
-                    os.path.abspath("tests/samples/not_a_file.root"): "Events",
-                }
-            }
-        }
-    else:
-        fileset = {"sample": {"files": {os.path.abspath("tests/samples/DYto2E.root"): "Events"}}}
+def test_tag_and_probe_electrons():
+    fileset = {"sample": {"files": {os.path.abspath("tests/samples/DYto2E.root"): "Events"}}}
 
-    if do_preprocess:
-        if allow_read_errors_with_report:
-            with pytest.raises(FileNotFoundError):
-                preprocess(fileset)
-            fileset_available, fileset_updated = preprocess(fileset, skip_bad_files=True)
-            fileset = fileset_available
-
-    tag_n_probe = ElePt_WPTight_Gsf(
+    tag_n_probe = ElectronTagNProbeFromNanoAOD(
         fileset,
-        32,
-        probes_pt_cut=29,
-        avoid_ecal_transition_tags=True,
-        avoid_ecal_transition_probes=True,
-        goldenjson=None,
+        True,
+        filter="Ele32",
+        trigger_pt=32,
+        filterbit=1,
+        cutbased_id="cutBased >= 4",
+        tags_pt_cut=35,
+        probes_pt_cut=27,
+        tags_abseta_cut=2.17,
+        use_sc_eta=True,
+        hlt_filter="Ele32_WPTight_Gsf",
     )
 
-    res = tag_n_probe.get_1d_pt_eta_phi_tnp_histograms(
-        uproot_options={"allow_read_errors_with_report": allow_read_errors_with_report},
-        compute=False,
-        scheduler=None,
-        progress=False,
-    )
-
-    if allow_read_errors_with_report:
-        histograms = res[0]["sample"]
-    else:
-        histograms = res["sample"]
-
-    hpt_pass_barrel, hpt_fail_barrel = histograms["pt"]["barrel"].values()
-    hpt_pass_endcap, hpt_fail_endcap = histograms["pt"]["endcap"].values()
-    heta_pass, heta_fail = histograms["eta"]["entire"].values()
-    hphi_pass, hphi_fail = histograms["phi"]["entire"].values()
-
-    assert hpt_pass_barrel.sum(flow=True).value + hpt_pass_endcap.sum(flow=True).value == 0.0
-    assert hpt_fail_barrel.sum(flow=True).value + hpt_fail_endcap.sum(flow=True).value == 0.0
-    assert heta_pass.sum(flow=True).value == 0.0
-    assert heta_fail.sum(flow=True).value == 0.0
-    assert hphi_pass.sum(flow=True).value == 0.0
-    assert hphi_fail.sum(flow=True).value == 0.0
-
-    assert hpt_pass_barrel.values(flow=True)[0] + hpt_pass_endcap.values(flow=True)[0] == 0.0
-    assert hpt_fail_barrel.values(flow=True)[0] + hpt_fail_endcap.values(flow=True)[0] == 0.0
-    assert heta_pass.values(flow=True)[0] == 0.0
-    assert heta_fail.values(flow=True)[0] == 0.0
-    assert hphi_pass.values(flow=True)[0] == 0.0
-    assert hphi_fail.values(flow=True)[0] == 0.0
+    events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/DYto2E.root"): "Events"}, delayed=False).events()
+    solution = tag_and_probe_electrons(events)
+    result = tag_n_probe.get_tnp_arrays(cut_and_count=False, vars=["Electron_pt", "tag_Ele_eta", "el_pt", "el_eta", "MET_pt", "event"], compute=True)["sample"]
+    assert_eq(result["passing"], solution[0])
+    assert_eq(result["failing"], solution[1])
 
 
-@pytest.mark.parametrize("do_preprocess", [True, False])
-@pytest.mark.parametrize("allow_read_errors_with_report", [True, False])
-def test_local_compute(do_preprocess, allow_read_errors_with_report):
-    if allow_read_errors_with_report:
-        fileset = {
-            "sample": {
-                "files": {
-                    os.path.abspath("tests/samples/DYto2E.root"): "Events",
-                    os.path.abspath("tests/samples/not_a_file.root"): "Events",
-                }
-            }
-        }
-    else:
-        fileset = {"sample": {"files": {os.path.abspath("tests/samples/DYto2E.root"): "Events"}}}
+@pytest.mark.parametrize("start_from_diphotons", [True, False])
+def test_tag_and_probe_photons(start_from_diphotons):
+    fileset = {"sample": {"files": {os.path.abspath("tests/samples/DYto2E.root"): "Events"}}}
 
-    if do_preprocess:
-        if allow_read_errors_with_report:
-            with pytest.raises(FileNotFoundError):
-                preprocess(fileset)
-            fileset_available, fileset_updated = preprocess(fileset, skip_bad_files=True)
-            fileset = fileset_available
-
-    tag_n_probe = ElePt_WPTight_Gsf(
+    tag_n_probe = PhotonTagNProbeFromNanoAOD(
         fileset,
-        32,
-        probes_pt_cut=29,
-        avoid_ecal_transition_tags=True,
-        avoid_ecal_transition_probes=True,
-        goldenjson=None,
+        True,
+        filter="Ele32",
+        is_electron_filter=True,
+        start_from_diphotons=start_from_diphotons,
+        cutbased_id="cutBased >= 3",
+        trigger_pt=32,
+        filterbit=1,
+        tags_pt_cut=35,
+        probes_pt_cut=27,
+        tags_abseta_cut=2.17,
+        use_sc_eta=False,
+        hlt_filter="Ele32_WPTight_Gsf",
     )
 
-    res = tag_n_probe.get_1d_pt_eta_phi_tnp_histograms(
-        uproot_options={"allow_read_errors_with_report": allow_read_errors_with_report},
-        compute=True,
-        scheduler=None,
-        progress=True,
-    )
-
-    if allow_read_errors_with_report:
-        histograms = res[0]["sample"]
-        report = res[1]["sample"]
-        if not do_preprocess:
-            assert report.exception[1] == "FileNotFoundError"
-    else:
-        histograms = res["sample"]
-
-    hpt_pass_barrel, hpt_fail_barrel = histograms["pt"]["barrel"].values()
-    hpt_pass_endcap, hpt_fail_endcap = histograms["pt"]["endcap"].values()
-    heta_pass, heta_fail = histograms["eta"]["entire"].values()
-    hphi_pass, hphi_fail = histograms["phi"]["entire"].values()
-
-    assert hpt_pass_barrel.sum(flow=True).value + hpt_pass_endcap.sum(flow=True).value == 954.0
-    assert hpt_fail_barrel.sum(flow=True).value + hpt_fail_endcap.sum(flow=True).value == 1153.0 - 954.0
-    assert heta_pass.sum(flow=True).value == 954.0
-    assert heta_fail.sum(flow=True).value == 1153.0 - 954.0
-    assert hphi_pass.sum(flow=True).value == 954.0
-    assert hphi_fail.sum(flow=True).value == 1153.0 - 954.0
-
-    assert hpt_pass_barrel.values(flow=True)[0] + hpt_pass_endcap.values(flow=True)[0] == 0.0
-    assert hpt_fail_barrel.values(flow=True)[0] + hpt_fail_endcap.values(flow=True)[0] == 0.0
-    assert heta_pass.values(flow=True)[0] == 0.0
-    assert heta_fail.values(flow=True)[0] == 0.0
-    assert hphi_pass.values(flow=True)[0] == 0.0
-    assert hphi_fail.values(flow=True)[0] == 0.0
-
-
-@pytest.mark.parametrize("do_preprocess", [True, False])
-@pytest.mark.parametrize("allow_read_errors_with_report", [True, False])
-def test_distributed_compute(do_preprocess, allow_read_errors_with_report):
-    from distributed import Client
-
-    if allow_read_errors_with_report:
-        fileset = {
-            "sample": {
-                "files": {
-                    os.path.abspath("tests/samples/DYto2E.root"): "Events",
-                    os.path.abspath("tests/samples/not_a_file.root"): "Events",
-                }
-            }
-        }
-    else:
-        fileset = {"sample": {"files": {os.path.abspath("tests/samples/DYto2E.root"): "Events"}}}
-
-    if do_preprocess:
-        if allow_read_errors_with_report:
-            with pytest.raises(FileNotFoundError):
-                preprocess(fileset)
-            fileset_available, fileset_updated = preprocess(fileset, skip_bad_files=True)
-            fileset = fileset_available
-
-    tag_n_probe = ElePt_WPTight_Gsf(
-        fileset,
-        32,
-        probes_pt_cut=29,
-        avoid_ecal_transition_tags=True,
-        avoid_ecal_transition_probes=True,
-        goldenjson=None,
-    )
-
-    with Client():
-        res = tag_n_probe.get_1d_pt_eta_phi_tnp_histograms(
-            uproot_options={"allow_read_errors_with_report": allow_read_errors_with_report},
-            compute=True,
-            scheduler=None,
-            progress=True,
-        )
-
-        if allow_read_errors_with_report:
-            histograms = res[0]["sample"]
-            report = res[1]["sample"]
-            if not do_preprocess:
-                assert report.exception[1] == "FileNotFoundError"
-        else:
-            histograms = res["sample"]
-
-        hpt_pass_barrel, hpt_fail_barrel = histograms["pt"]["barrel"].values()
-        hpt_pass_endcap, hpt_fail_endcap = histograms["pt"]["endcap"].values()
-        heta_pass, heta_fail = histograms["eta"]["entire"].values()
-        hphi_pass, hphi_fail = histograms["phi"]["entire"].values()
-
-        assert hpt_pass_barrel.sum(flow=True).value + hpt_pass_endcap.sum(flow=True).value == 954.0
-        assert hpt_fail_barrel.sum(flow=True).value + hpt_fail_endcap.sum(flow=True).value == 1153.0 - 954.0
-        assert heta_pass.sum(flow=True).value == 954.0
-        assert heta_fail.sum(flow=True).value == 1153.0 - 954.0
-        assert hphi_pass.sum(flow=True).value == 954.0
-        assert hphi_fail.sum(flow=True).value == 1153.0 - 954.0
-
-        assert hpt_pass_barrel.values(flow=True)[0] + hpt_pass_endcap.values(flow=True)[0] == 0.0
-        assert hpt_fail_barrel.values(flow=True)[0] + hpt_fail_endcap.values(flow=True)[0] == 0.0
-        assert heta_pass.values(flow=True)[0] == 0.0
-        assert heta_fail.values(flow=True)[0] == 0.0
-        assert hphi_pass.values(flow=True)[0] == 0.0
-        assert hphi_fail.values(flow=True)[0] == 0.0
+    events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/DYto2E.root"): "Events"}, delayed=False).events()
+    solution = tag_and_probe_photons(events, start_from_diphotons)
+    result = tag_n_probe.get_tnp_arrays(cut_and_count=False, vars=["Photon_pt", "tag_Ele_eta", "ph_pt", "ph_eta", "MET_pt", "event"], compute=True)["sample"]
+    assert_eq(result["passing"], solution[0])
+    assert_eq(result["failing"], solution[1])
--- tests/test_tag_and_probe_ntuples.py
+++ tests/test_tag_and_probe_ntuples.py
@@ -2,208 +2,48 @@ from __future__ import annotations
 
 import os
 
-import pytest
-from coffea.dataset_tools import preprocess
+from coffea.nanoevents import BaseSchema, NanoEventsFactory
+from dask_awkward.lib.testutils import assert_eq
+from dummy_tag_and_probe_ntuples import tag_and_probe_electrons, tag_and_probe_photons
 
-from egamma_tnp.triggers import ElePt_WPTight_Gsf
+from egamma_tnp import ElectronTagNProbeFromNTuples, PhotonTagNProbeFromNTuples
 
 
-@pytest.mark.parametrize("do_preprocess", [True, False])
-@pytest.mark.parametrize("allow_read_errors_with_report", [True, False])
-def test_without_compute(do_preprocess, allow_read_errors_with_report):
-    if allow_read_errors_with_report:
-        fileset = {
-            "sample": {
-                "files": {
-                    os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree",
-                    os.path.abspath("tests/samples/not_a_file.root"): "fitter_tree",
-                }
-            }
-        }
-    else:
-        fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree"}}}
+def test_tag_and_probe_electrons():
+    fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree"}}}
 
-    if do_preprocess:
-        if allow_read_errors_with_report:
-            with pytest.raises(FileNotFoundError):
-                preprocess(fileset)
-            fileset_available, fileset_updated = preprocess(fileset, skip_bad_files=True)
-            fileset = fileset_available
-
-    tag_n_probe = ElePt_WPTight_Gsf(
+    tag_n_probe = ElectronTagNProbeFromNTuples(
         fileset,
-        trigger_pt=32,
-        from_ntuples=True,
+        "passingCutBasedTight122XV1",
+        cutbased_id="passingCutBasedLoose122XV1",
+        use_sc_eta=True,
         tags_pt_cut=35,
-        probes_pt_cut=5,
-        use_sc_eta=False,
-        avoid_ecal_transition_tags=False,
+        probes_pt_cut=27,
+        tags_abseta_cut=2.17,
     )
 
-    res = tag_n_probe.get_1d_pt_eta_phi_tnp_histograms(
-        uproot_options={"allow_read_errors_with_report": allow_read_errors_with_report},
-        compute=False,
-        scheduler=None,
-        progress=False,
-    )
-
-    if allow_read_errors_with_report:
-        histograms = res[0]["sample"]
-    else:
-        histograms = res["sample"]
-
-    hpt_pass_barrel, hpt_fail_barrel = histograms["pt"]["barrel"].values()
-    hpt_pass_endcap, hpt_fail_endcap = histograms["pt"]["endcap"].values()
-    heta_pass, heta_fail = histograms["eta"]["entire"].values()
-    hphi_pass, hphi_fail = histograms["phi"]["entire"].values()
-
-    assert hpt_pass_barrel.sum(flow=True).value + hpt_pass_endcap.sum(flow=True).value == 0.0
-    assert hpt_fail_barrel.sum(flow=True).value + hpt_fail_endcap.sum(flow=True).value == 0.0
-    assert heta_pass.sum(flow=True).value == 0.0
-    assert heta_fail.sum(flow=True).value == 0.0
-    assert hphi_pass.sum(flow=True).value == 0.0
-    assert hphi_fail.sum(flow=True).value == 0.0
-
-    assert hpt_pass_barrel.values(flow=True)[0] + hpt_pass_endcap.values(flow=True)[0] == 0.0
-    assert hpt_fail_barrel.values(flow=True)[0] + hpt_fail_endcap.values(flow=True)[0] == 0.0
-    assert heta_pass.values(flow=True)[0] == 0.0
-    assert heta_fail.values(flow=True)[0] == 0.0
-    assert hphi_pass.values(flow=True)[0] == 0.0
-    assert hphi_fail.values(flow=True)[0] == 0.0
+    events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree"}, schemaclass=BaseSchema, delayed=False).events()
+    solution = tag_and_probe_electrons(events)
+    result = tag_n_probe.get_tnp_arrays(cut_and_count=False, vars=["el_pt", "el_eta", "truePU", "tag_Ele_eta"], compute=True)["sample"]
+    assert_eq(result["passing"], solution[0])
+    assert_eq(result["failing"], solution[1])
 
 
-@pytest.mark.parametrize("do_preprocess", [True, False])
-@pytest.mark.parametrize("allow_read_errors_with_report", [True, False])
-def test_local_compute(do_preprocess, allow_read_errors_with_report):
-    if allow_read_errors_with_report:
-        fileset = {
-            "sample": {
-                "files": {
-                    os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree",
-                    os.path.abspath("tests/samples/not_a_file.root"): "fitter_tree",
-                }
-            }
-        }
-    else:
-        fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree"}}}
+def test_tag_and_probe_photons():
+    fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples_ph.root"): "fitter_tree"}}}
 
-    if do_preprocess:
-        if allow_read_errors_with_report:
-            with pytest.raises(FileNotFoundError):
-                preprocess(fileset)
-            fileset_available, fileset_updated = preprocess(fileset, skip_bad_files=True)
-            fileset = fileset_available
-
-    tag_n_probe = ElePt_WPTight_Gsf(
+    tag_n_probe = PhotonTagNProbeFromNTuples(
         fileset,
-        trigger_pt=32,
-        from_ntuples=True,
+        "passingCutBasedTight122XV1",
+        cutbased_id="passingCutBasedLoose122XV1",
+        use_sc_eta=True,
         tags_pt_cut=35,
-        probes_pt_cut=5,
-        use_sc_eta=False,
-        avoid_ecal_transition_tags=False,
+        probes_pt_cut=27,
+        tags_abseta_cut=2.17,
     )
 
-    res = tag_n_probe.get_1d_pt_eta_phi_tnp_histograms(
-        uproot_options={"allow_read_errors_with_report": allow_read_errors_with_report},
-        compute=True,
-        scheduler=None,
-        progress=True,
-    )
-
-    if allow_read_errors_with_report:
-        histograms = res[0]["sample"]
-        report = res[1]["sample"]
-        if not do_preprocess:
-            assert report.exception[1] == "FileNotFoundError"
-    else:
-        histograms = res["sample"]
-
-    hpt_pass_barrel, hpt_fail_barrel = histograms["pt"]["barrel"].values()
-    hpt_pass_endcap, hpt_fail_endcap = histograms["pt"]["endcap"].values()
-    heta_pass, heta_fail = histograms["eta"]["entire"].values()
-    hphi_pass, hphi_fail = histograms["phi"]["entire"].values()
-
-    assert hpt_pass_barrel.sum(flow=True).value + hpt_pass_endcap.sum(flow=True).value == 349.0
-    assert hpt_fail_barrel.sum(flow=True).value + hpt_fail_endcap.sum(flow=True).value == 490.0 - 349.0
-    assert heta_pass.sum(flow=True).value == 361.0
-    assert heta_fail.sum(flow=True).value == 505.0 - 361.0
-    assert hphi_pass.sum(flow=True).value == 361.0
-    assert hphi_fail.sum(flow=True).value == 505.0 - 361.0
-
-    assert hpt_pass_barrel.values(flow=True)[0] + hpt_pass_endcap.values(flow=True)[0] == 0.0
-    assert hpt_fail_barrel.values(flow=True)[0] + hpt_fail_endcap.values(flow=True)[0] == 0.0
-    assert heta_pass.values(flow=True)[0] == 0.0
-    assert heta_fail.values(flow=True)[0] == 0.0
-    assert hphi_pass.values(flow=True)[0] == 0.0
-    assert hphi_fail.values(flow=True)[0] == 0.0
-
-
-@pytest.mark.parametrize("do_preprocess", [True, False])
-@pytest.mark.parametrize("allow_read_errors_with_report", [True, False])
-def test_distributed_compute(do_preprocess, allow_read_errors_with_report):
-    from distributed import Client
-
-    if allow_read_errors_with_report:
-        fileset = {
-            "sample": {
-                "files": {
-                    os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree",
-                    os.path.abspath("tests/samples/not_a_file.root"): "fitter_tree",
-                }
-            }
-        }
-    else:
-        fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree"}}}
-
-    if do_preprocess:
-        if allow_read_errors_with_report:
-            with pytest.raises(FileNotFoundError):
-                preprocess(fileset)
-            fileset_available, fileset_updated = preprocess(fileset, skip_bad_files=True)
-            fileset = fileset_available
-
-    tag_n_probe = ElePt_WPTight_Gsf(
-        fileset,
-        trigger_pt=32,
-        from_ntuples=True,
-        tags_pt_cut=35,
-        probes_pt_cut=5,
-        use_sc_eta=False,
-        avoid_ecal_transition_tags=False,
-    )
-
-    with Client():
-        res = tag_n_probe.get_1d_pt_eta_phi_tnp_histograms(
-            uproot_options={"allow_read_errors_with_report": allow_read_errors_with_report},
-            compute=True,
-            scheduler=None,
-            progress=True,
-        )
-
-        if allow_read_errors_with_report:
-            histograms = res[0]["sample"]
-            report = res[1]["sample"]
-            if not do_preprocess:
-                assert report.exception[1] == "FileNotFoundError"
-        else:
-            histograms = res["sample"]
-
-        hpt_pass_barrel, hpt_fail_barrel = histograms["pt"]["barrel"].values()
-        hpt_pass_endcap, hpt_fail_endcap = histograms["pt"]["endcap"].values()
-        heta_pass, heta_fail = histograms["eta"]["entire"].values()
-        hphi_pass, hphi_fail = histograms["phi"]["entire"].values()
-
-        assert hpt_pass_barrel.sum(flow=True).value + hpt_pass_endcap.sum(flow=True).value == 349.0
-        assert hpt_fail_barrel.sum(flow=True).value + hpt_fail_endcap.sum(flow=True).value == 490.0 - 349.0
-        assert heta_pass.sum(flow=True).value == 361.0
-        assert heta_fail.sum(flow=True).value == 505.0 - 361.0
-        assert hphi_pass.sum(flow=True).value == 361.0
-        assert hphi_fail.sum(flow=True).value == 505.0 - 361.0
-
-        assert hpt_pass_barrel.values(flow=True)[0] + hpt_pass_endcap.values(flow=True)[0] == 0.0
-        assert hpt_fail_barrel.values(flow=True)[0] + hpt_fail_endcap.values(flow=True)[0] == 0.0
-        assert heta_pass.values(flow=True)[0] == 0.0
-        assert heta_fail.values(flow=True)[0] == 0.0
-        assert hphi_pass.values(flow=True)[0] == 0.0
-        assert hphi_fail.values(flow=True)[0] == 0.0
+    events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/TnPNTuples_ph.root"): "fitter_tree"}, schemaclass=BaseSchema, delayed=False).events()
+    solution = tag_and_probe_photons(events)
+    result = tag_n_probe.get_tnp_arrays(cut_and_count=False, vars=["ph_et", "ph_eta", "truePU", "tag_Ele_eta"], compute=True)["sample"]
+    assert_eq(result["passing"], solution[0])
+    assert_eq(result["failing"], solution[1])

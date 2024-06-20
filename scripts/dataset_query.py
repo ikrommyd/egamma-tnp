91a5b4c9874f77698e63ac766df151bcdeccb609
Iason Krommydas <iason.krom@gmail.com>
2024-06-21 00:49:23 +0200

    add dataset_query as a shell command

---
+++ scripts/dataset_query.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from __future__ import annotations
+
+import subprocess
+import sys
+
+
+def main():
+    # Forward the arguments to the coffea.dataset_tools.dataset_query module
+    subprocess.run([sys.executable, "-m", "coffea.dataset_tools.dataset_query"] + sys.argv[1:], check=False)
+
+
+if __name__ == "__main__":
+    main()

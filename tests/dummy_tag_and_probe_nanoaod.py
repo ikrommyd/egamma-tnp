2de7df3bf5e37740d9bb00ed729c7cfa90707fb4
Iason Krommydas <iason.krom@gmail.com>
2024-09-07 12:58:28 -0500

    temporary fix for vector issue #498

--- tests/dummy_tag_and_probe_nanoaod.py
+++ tests/dummy_tag_and_probe_nanoaod.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import awkward as ak
+from coffea.nanoevents.methods import candidate
 
 from egamma_tnp.utils import custom_delta_r
 
@@ -121,6 +122,14 @@ def tag_and_probe_electrons(events, is_id):
 
 
 def tag_and_probe_photons(events, start_from_diphotons, is_id):
+    # TODO: remove this temporary fix when https://github.com/scikit-hep/vector/issues/498 is resolved
+    photon_dict = {field: events.Photon[field] for field in events.Photon.fields} | {
+        "mass": ak.zeros_like(events.Photon.pt),
+        "charge": ak.zeros_like(events.Photon.pt),
+    }
+
+    events["Photon"] = ak.zip(photon_dict, with_name="Photon", behavior=candidate.behavior)
+
     events["Photon", "eta_to_use"] = events.Photon.eta
     events["Photon", "phi_to_use"] = events.Photon.phi
     events["Electron", "eta_to_use"] = events.Electron.eta

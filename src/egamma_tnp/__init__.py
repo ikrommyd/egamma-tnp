f691c4a8c36a1ae81ad2fc4490cf07424e37ed2c
Iason Krommydas <iason.krom@gmail.com>
2024-09-09 15:00:29 -0500

    maybe handle remote paths in merge_parquet

--- src/egamma_tnp/__init__.py
+++ src/egamma_tnp/__init__.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning, module="coffea.*")
+
 from egamma_tnp.config import binning
 from egamma_tnp.nanoaod_efficiency import ElectronTagNProbeFromNanoAOD, PhotonTagNProbeFromNanoAOD
 from egamma_tnp.ntuple_efficiency import ElectronTagNProbeFromNTuples, PhotonTagNProbeFromNTuples
 
 from . import _version
 
+warnings.filterwarnings("ignore", category=FutureWarning, module="coffea.*")
 __version__ = _version.__version__
 __all__ = ("binning", "ElectronTagNProbeFromNTuples", "ElectronTagNProbeFromNanoAOD", "PhotonTagNProbeFromNTuples", "PhotonTagNProbeFromNanoAOD")
 

f63b2ff596ea55546c9d7627bf74e3036c59bc83
Iason Krommydas <iason.krom@gmail.com>
2024-09-15 17:31:27 -0500

    add testing template

--- tests/test_tag_and_probe_ntuples.py
+++ tests/test_tag_and_probe_ntuples.py
@@ -4,12 +4,12 @@ import os
 
 import awkward as ak
 from coffea.nanoevents import BaseSchema, NanoEventsFactory
-from dummy_tag_and_probe_ntuples import tag_and_probe_electrons, tag_and_probe_photons
+from dummy_tag_and_probe_ntuples import mini_tag_and_probe_electrons, mini_tag_and_probe_photons, nano_tag_and_probe_electrons, nano_tag_and_probe_photons
 
 from egamma_tnp import ElectronTagNProbeFromMiniNTuples, PhotonTagNProbeFromMiniNTuples
 
 
-def test_tag_and_probe_electrons():
+def test_mini_tag_and_probe_electrons():
     fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree"}}}
 
     tag_n_probe = ElectronTagNProbeFromMiniNTuples(
@@ -23,7 +23,7 @@ def test_tag_and_probe_electrons():
     )
 
     events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/TnPNTuples_el.root"): "fitter_tree"}, schemaclass=BaseSchema, delayed=False).events()
-    solution = tag_and_probe_electrons(events)
+    solution = mini_tag_and_probe_electrons(events)
     result = tag_n_probe.get_passing_and_failing_probes(
         "passingCutBasedTight122XV1", cut_and_count=False, vars=["el_pt", "el_eta", "truePU", "tag_Ele_eta"], compute=True
     )["sample"]
@@ -35,7 +35,7 @@ def test_tag_and_probe_electrons():
     assert len(solution[1]) == 113
 
 
-def test_tag_and_probe_photons():
+def test_mini_tag_and_probe_photons():
     fileset = {"sample": {"files": {os.path.abspath("tests/samples/TnPNTuples_ph.root"): "fitter_tree"}}}
 
     tag_n_probe = PhotonTagNProbeFromMiniNTuples(
@@ -49,7 +49,59 @@ def test_tag_and_probe_photons():
     )
 
     events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/TnPNTuples_ph.root"): "fitter_tree"}, schemaclass=BaseSchema, delayed=False).events()
-    solution = tag_and_probe_photons(events)
+    solution = mini_tag_and_probe_photons(events)
+    result = tag_n_probe.get_passing_and_failing_probes(
+        "passingCutBasedTight122XV1", cut_and_count=False, vars=["ph_et", "ph_eta", "truePU", "tag_Ele_eta"], compute=True
+    )["sample"]
+    ak.array_equal(result["passing"], solution[0])
+    ak.array_equal(result["failing"], solution[1])
+    assert len(result["passing"]) == 372
+    assert len(result["failing"]) == 73
+    assert len(solution[0]) == 372
+    assert len(solution[1]) == 73
+
+
+def test_nano_tag_and_probe_electrons():
+    fileset = {"sample": {"files": {os.path.abspath("tests/samples/NanoNTuples_el.root"): "fitter_tree"}}}
+
+    tag_n_probe = ElectronTagNProbeFromMiniNTuples(
+        fileset,
+        ["passingCutBasedTight122XV1"],
+        cutbased_id="passingCutBasedLoose122XV1",
+        use_sc_eta=True,
+        tags_pt_cut=35,
+        probes_pt_cut=27,
+        tags_abseta_cut=2.17,
+    )
+
+    events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/NanoNTuples_el.root"): "fitter_tree"}, schemaclass=BaseSchema, delayed=False).events()
+    solution = nano_tag_and_probe_electrons(events)
+    result = tag_n_probe.get_passing_and_failing_probes(
+        "passingCutBasedTight122XV1", cut_and_count=False, vars=["el_pt", "el_eta", "truePU", "tag_Ele_eta"], compute=True
+    )["sample"]
+    ak.array_equal(result["passing"], solution[0])
+    ak.array_equal(result["failing"], solution[1])
+    assert len(result["passing"]) == 414
+    assert len(result["failing"]) == 113
+    assert len(solution[0]) == 414
+    assert len(solution[1]) == 113
+
+
+def test_nano_tag_and_probe_photons():
+    fileset = {"sample": {"files": {os.path.abspath("tests/samples/NanoNTuples_ph.root"): "fitter_tree"}}}
+
+    tag_n_probe = PhotonTagNProbeFromMiniNTuples(
+        fileset,
+        ["passingCutBasedTight122XV1"],
+        cutbased_id="passingCutBasedLoose122XV1",
+        use_sc_eta=True,
+        tags_pt_cut=35,
+        probes_pt_cut=27,
+        tags_abseta_cut=2.17,
+    )
+
+    events = NanoEventsFactory.from_root({os.path.abspath("tests/samples/NanoNTuples_ph.root"): "fitter_tree"}, schemaclass=BaseSchema, delayed=False).events()
+    solution = nano_tag_and_probe_photons(events)
     result = tag_n_probe.get_passing_and_failing_probes(
         "passingCutBasedTight122XV1", cut_and_count=False, vars=["ph_et", "ph_eta", "truePU", "tag_Ele_eta"], compute=True
     )["sample"]

0df5511e40a598c20a5604fc3c034a668f1b2f28
Iason Krommydas <iason.krom@gmail.com>
2024-09-18 12:35:54 -0500

    fix bit changing in nanoaod v13

--- src/egamma_tnp/nanoaod_efficiency.py
+++ src/egamma_tnp/nanoaod_efficiency.py
@@ -332,7 +332,7 @@ class ElectronTagNProbeFromNanoAOD(BaseTagNProbe):
                 doclist = [x for x in good_events.TrigObj.filterBits.__doc__.split(";") if x.endswith("for Electron")]
                 eledoc = doclist[0] if doclist else None
                 if eledoc is not None:
-                    if bit == 12 and "Leg 1" not in eledoc and "Leg 2" not in eledoc:
+                    if (bit == 12) and (trigger_pt in [115, 135]) and ("Leg 1" not in eledoc) and ("Leg 2" not in eledoc):
                         import warnings
 
                         warnings.warn(

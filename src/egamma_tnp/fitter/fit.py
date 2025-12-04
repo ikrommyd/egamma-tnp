fcbf8d24fc0dce44c385da45e2f0e2e2fa987b38
Iason Krommydas <iason.krom@gmail.com>
2025-12-04 13:20:02 -0600

    use single signal fraction (bkg = 1-sig)

--- src/egamma_tnp/fitter/fit.py
+++ src/egamma_tnp/fitter/fit.py
@@ -22,9 +22,13 @@ class Fitter:
 
         self._sig_params = list(config["signal"].keys())
         self._bkg_params = list(config["background"].keys())
-        # Always use sig_frac and bkg_frac as yield parameters
-        self._yield_params = ["sig_frac", "bkg_frac"]
+        # Use only n_sig; n_bkg is computed as total_variance - n_sig
+        self._yield_params = ["n_sig"]
         self._all_params = self._sig_params + self._bkg_params + self._yield_params
+
+        # Calculate total variance first (needed for model_pdf closure)
+        hist_sliced_temp = hist[fit_range[0] * 1j : fit_range[1] * 1j]
+        total_variance = hist_sliced_temp.variances().sum()
 
         def model_pdf(xe, *args):
             params = dict(zip(self._all_params, args))
@@ -35,7 +39,10 @@ class Fitter:
             sig_pdf = self._signal_pdf.pdf(xe, **sig_kwargs)
             bkg_pdf = self._background_pdf.pdf(xe, **bkg_kwargs)
 
-            return params["sig_frac"] * sig_pdf + params["bkg_frac"] * bkg_pdf
+            # n_sig is the signal count; n_bkg = total_variance - n_sig
+            n_sig = params["n_sig"]
+            n_bkg = total_variance - n_sig
+            return n_sig * sig_pdf + n_bkg * bkg_pdf
 
         # Tell iminuit the parameter names using __signature__
         params_signature = [Parameter("xe", Parameter.POSITIONAL_OR_KEYWORD)]
@@ -51,7 +58,7 @@ class Fitter:
             for i in range(len(self._bin_edges) - 1):
                 a = self._bin_edges[i]
                 b = self._bin_edges[i + 1]
-                x_points = np.linspace(a, b, 51)
+                x_points = np.linspace(a, b, 11)
                 y_points = model_pdf(x_points, *args)
                 d[i] = simpson(y_points, x=x_points)
             return d
@@ -71,39 +78,35 @@ class Fitter:
             if "limits" in param_config:
                 limits[param_name] = param_config["limits"]
 
-        # Calculate total sum of variances for yield normalization
-        total_variance = hist_sliced.variances().sum()
-
         # Handle yield parameters with default values and fraction-based initialization
+        # User specifies sig_frac (0-1), which gets converted to n_sig (actual count)
         yield_defaults = {
-            "sig_frac": {"init": 0.5, "limits": (0, 1.2)},
-            "bkg_frac": {"init": 0.5, "limits": (0, 1.2)},
+            "sig_frac": {"init": 0.5, "limits": (0, 1)},
         }
 
         # Get yield config or use empty dict if not provided
         yields_config = config.get("yields", {})
 
-        for param_name in ["sig_frac", "bkg_frac"]:
-            # Get user config or default
-            param_config = yields_config.get(param_name, yield_defaults[param_name])
+        # Handle n_sig parameter (signal count)
+        sig_frac_config = yields_config.get("sig_frac", yield_defaults["sig_frac"])
 
-            # Convert fraction to actual value
-            init_fraction = param_config.get("init", yield_defaults[param_name]["init"])
-            init_values[param_name] = init_fraction * total_variance
+        # Convert sig_frac fraction to actual n_sig count
+        init_sig_frac = sig_frac_config.get("init", yield_defaults["sig_frac"]["init"])
+        init_values["n_sig"] = init_sig_frac * total_variance
 
-            # Convert limit fractions to actual values
-            if "limits" in param_config:
-                limit_fractions = param_config["limits"]
-                limits[param_name] = (
-                    limit_fractions[0] * total_variance,
-                    limit_fractions[1] * total_variance,
-                )
-            else:
-                default_limits = yield_defaults[param_name]["limits"]
-                limits[param_name] = (
-                    default_limits[0] * total_variance,
-                    default_limits[1] * total_variance,
-                )
+        # Convert limit fractions to actual values
+        if "limits" in sig_frac_config:
+            limit_fractions = sig_frac_config["limits"]
+            limits["n_sig"] = (
+                limit_fractions[0] * total_variance,
+                limit_fractions[1] * total_variance,
+            )
+        else:
+            default_limits = yield_defaults["sig_frac"]["limits"]
+            limits["n_sig"] = (
+                default_limits[0] * total_variance,
+                default_limits[1] * total_variance,
+            )
 
         self.minuit = Minuit(cost, **init_values)
         self.minuit.strategy = 2

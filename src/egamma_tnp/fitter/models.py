b5bcf63a864d202b94b83fdac84a987cfcfceaeb
Iason Krommydas <iason.krom@gmail.com>
2025-12-04 15:01:34 -0600

    add normal and cmsshape pdfs

--- src/egamma_tnp/fitter/models.py
+++ src/egamma_tnp/fitter/models.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
 
-from numba_stats import crystalball_ex, expon
+from numba_stats import cmsshape, crystalball_ex, expon, norm
 
 from egamma_tnp.fitter.basepdf import BasePDF, register_pdf
+
+
+# Define Normal (Gaussian) PDF
+@register_pdf("Normal")
+class Normal(BasePDF):
+    def _unnormalized_pdf(self, x, mu, sigma):
+        return norm.pdf(x, mu, sigma)
+
+    def _unnormalized_cdf(self, x, mu, sigma):
+        return norm.cdf(x, mu, sigma)
 
 
 # Define DoubleCrystalBall PDF
@@ -23,3 +33,13 @@ class Exponential(BasePDF):
 
     def _unnormalized_cdf(self, x, lambd):
         return expon.cdf(x, self.norm_range[0], lambd)
+
+
+# Define CMSShape PDF
+@register_pdf("CMSShape")
+class CMSShape(BasePDF):
+    def _unnormalized_pdf(self, x, beta, gamma, loc):
+        return cmsshape.pdf(x, beta, gamma, loc)
+
+    def _unnormalized_cdf(self, x, beta, gamma, loc):
+        return cmsshape.cdf(x, beta, gamma, loc)
